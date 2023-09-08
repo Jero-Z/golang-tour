92e74c386aa311867c1bb8073067479b17499693
Jero <jun771480011@gmail.com>
2023-09-08 17:33:01 +0800

    main

--- main.go
+++ main.go
@@ -3,11 +3,5 @@ package main
 import "fmt"
 
 func main() {
-
-	var pMap = make(map[int]bool, 10)
-	var s = []int{1, 2, 3, 4}
-	for _, b := range s {
-		pMap[b] = false
-	}
-	fmt.Println(pMap)
+	fmt.Println("gin-tour")
 }
